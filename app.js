5f5cb2ffb207e4fb3119af6dce62a7e1d227918b
TinaLindhqvist <TinaLindhqvist@users.noreply.github.com>
2017-10-31 14:06:56 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,7 @@ var cfenv = require('cfenv');
 var app = express();
 app.use(app.router);
 app.use(express.errorHandler());
-app.set('view engine', 'jade');
-app.get('/', function(req, res){
-	res.render('index');
-});
+app.use(express.static(__dirname + '/public'));
 var appEnv = cfenv.getAppEnv();
 app.listen(appEnv.port, '0.0.0.0', function() {
   console.log("server starting on " + appEnv.url);
